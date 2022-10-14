78f08b142907f5c9c772c85c540ae4364d6eb2aa
Lijuan Xu <lijuanxu@hotmail.com>
2022-10-14 12:37:37 -0500

    authenticate me frontend: phase 1

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,4 @@
+// frontend/src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
@@ -6,18 +7,16 @@ import './index.css';
 import App from './App';
 import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
+import * as sessionActions from './store/session';
 
 const store = configureStore();
-
-if (process.env.NODE_ENV !== "production") {
-  window.store = store;
-}
 
 if (process.env.NODE_ENV !== 'production') {
   restoreCSRF();
 
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 function Root() {
