8642d96e7b3359fa49f6ba8db11a02e8e920f52a
Lijuan Xu <lijuanxu@hotmail.com>
2023-02-12 12:02:50 -0600

    component-Maps

--- frontend/src/components/Maps/Maps.js
+++ frontend/src/components/Maps/Maps.js
@@ -1,20 +1,95 @@
 import { useSelector } from 'react-redux';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom'
+import { GoogleMap, useJsApiLoader, OverlayView } from '@react-google-maps/api';
 
-import Maps from './Maps';
+const containerStyle = {
+    width: '100%',
+    height: '100%',
+}
+
+
 import './Maps.css';
 
-const MapContainer = ({ setQuery, setCenter, center }) => {
-    const key = useSelector((state) => state.maps.key);
+const Maps = ({ setQuery, setCenter, center }) => {
+    const key = useSelector((state) => state.mapState.key);
 
     if (!key) {
         return null;
     }
 
+    const [libraries] = useState(['places']);
+    const [mapref, setMapRef] = React.useState(null);
+
+    const handleOnLoad = map => {
+        setMapRef(map);
+    };
+
+    const handleCenterChanged = () => {
+        if (mapref) {
+            const newBound = mapref.getBounds();
+            const newCenter = mapref.getCenter();
+            // console.log('NE', newBound.getNorthEast().lat(), newBound.getNorthEast().lng());
+            // console.log('SW', newBound.getSouthWest().lat(), newBound.getSouthWest().lng());
+            // console.log('------------- newCenter', newCenter.lat(), newCenter.lng())
+            if (newCenter.lat() === center.lat && newCenter.lng() === center.lng) return;
+
+            setQuery(query => {
+                const newQuery = {}
+                newQuery.minLat = newBound.getSouthWest().lat();
+                newQuery.maxLat = newBound.getNorthEast().lat();
+                newQuery.minLng = newBound.getSouthWest().lng();
+                newQuery.maxLng = newBound.getNorthEast().lng();
+
+                return newQuery;
+            });
+        }
+    };
+
+    const spots = useSelector(state => state.spotState.allSpots);
+
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: key,
+        libraries
+    });
+
+    let spotsArr = [];
+    if (spots) spotsArr = Object.values(spots);
+
+    const history = useHistory();
+    const handleClick = (spotId) => {
+        history.push(`/spots/${spotId}`)
+    }
+
     return (
         <div className='home-page-map-container'>
-            <Maps apiKey={key} setQuery={setQuery} setCenter={setCenter} center={center} />
+            <div className='google-map'>
+            {isLoaded && (
+                <GoogleMap
+                    mapContainerStyle={containerStyle}
+                    center={center}
+                    zoom={11}
+                    onLoad={handleOnLoad}
+                    onCenterChanged={handleCenterChanged}
+                >
+                {spotsArr.map(spot => {
+                    return (
+                        <div key={spot.id} className='overlay-container'>
+                            <OverlayView
+                                position={{ lat: spot.lat, lng: spot.lng }}
+                                mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
+                            >
+                                <button onClick={() => handleClick(spot.id)} className='overlay-button'>${parseFloat(spot.price).toFixed(0).toLocaleString()}</button>
+                            </OverlayView>
+                        </div>
+                    )
+                })}
+                </GoogleMap>
+            )}
+        </div>
         </div>
     );
 };
 
-export default MapContainer;
+export default Maps;
