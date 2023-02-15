2c3c9682a533bf5a10a781990a9b892d3123e8b1
Lijuan Xu <lijuanxu@hotmail.com>
2023-02-15 10:59:48 -0600

    dropdown menu css

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -51,19 +51,29 @@ function ProfileButton ({ user }) {
         </button>
         {showMenu && (
           <ul className="profile-dropdown">
-            <li>{user.username}</li>
-            <li>{user.email}</li>
-            <li>
-              <NavLink style={{ color: 'black'}} key='listing' to='/spots/current'>My Listings</NavLink>
+            <li><i class="fa-solid fa-user"></i>{user.username}</li>
+            <li><i class="fa-solid fa-envelope"></i>{user.email}</li>
+            <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+                      <div style={{flex: 1, height: '1px', backgroundColor: '#dddddd'}} />
+            </div>
+            <li className="ul-link">
+              <i class="fa-solid fa-plane"></i>
+              <NavLink style={{ color: 'black', textDecoration:'none'}} key='review' to='/bookings/current'><strong>Trips</strong></NavLink>
             </li>
-            <li>
-              <NavLink style={{ color: 'black'}} key='review' to='/bookings/current'>Trips</NavLink>
+            <li className="ul-link">
+              <i class="fa-solid fa-house"></i>
+              <NavLink style={{ color: 'black', textDecoration:'none'}} key='listing' to='/spots/current'><strong>Listings</strong></NavLink>
             </li>
-            <li>
-              <NavLink style={{ color: 'black'}} key='review' to='/reviews/current'>My Reviews</NavLink>
+            <li className="ul-link">
+              <i class="fa-solid fa-pen-to-square"></i>
+              <NavLink style={{ color: 'black', textDecoration:'none'}} key='review' to='/reviews/current'><strong>Reviews</strong></NavLink>
             </li>
-            <li>
-              <button id='logout-button' onClick={logout}>Log Out</button>
+            <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+                      <div style={{flex: 1, height: '1px', backgroundColor: '#dddddd'}} />
+            </div>
+            <li className="ul-link">
+              <i class="fa-solid fa-right-from-bracket"></i>
+              <button className='logout-button' id='logout-btn' onClick={logout}>Log Out</button>
             </li>
           </ul>
         )}
